c0b77bca7e530c50a1021c0fea1a21797a3096c0
chaimaab185 <thankyouimad@gmail.com>
2024-12-18 01:24:17 +0100

    Update fichier.js

--- src/fichier.js
+++ src/fichier.js
@@ -1,5 +1,13 @@
-function moyenne(a,b){
-    moy=(a+b)/2;
-    return moy;
+// src/fichier.js
+import React from 'react';
+
+function Fichier() {
+    return (
+        <div>
+            <h2>Bienvenue dans mon application React!</h2>
+            <p>Ce message provient du fichier `fichier.js`.</p>
+        </div>
+    );
 }
-console.log(moyenne(d,v));
+
+export default Fichier;
